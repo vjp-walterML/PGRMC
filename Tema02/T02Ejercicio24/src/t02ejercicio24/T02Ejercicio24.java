94a8fe8ea023b4521b1058ba3ee62619a87ad33c
1DAW - Walter Martin Lopes <wmartinl01@iesvjp.es>
2022-11-18 17:10:06 +0100

    Correcciones

--- Tema02/T02Ejercicio24/src/t02ejercicio24/T02Ejercicio24.java
+++ Tema02/T02Ejercicio24/src/t02ejercicio24/T02Ejercicio24.java
@@ -15,21 +15,32 @@ public class T02Ejercicio24 {
      */
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        //Declaro las variables
         float pgrmc,lmsgi,bbdd,entdl,ssinf,fol,notaMedia;
+        
+        //Pido las notas de cada asignatura
         System.out.println("Por favor, introduzca la nota de Programación:");
         pgrmc = entrada.nextFloat();
+        
         System.out.println("Introduzca la nota de Lenguajes de Marcas:");
         lmsgi = entrada.nextFloat();
+        
         System.out.println("Introduzca la nota de Bases de Datos:");
         bbdd = entrada.nextFloat();
+        
         System.out.println("Introduzca la nota de Entornos de Desarrollo:");
         entdl = entrada.nextFloat();
+        
         System.out.println("Introduzca la nota de Sistemas Informáticos:");
         ssinf = entrada.nextFloat();
+        
         System.out.println("Por último, introduzca la nota de Formación y Orientación Laboral:");
         fol = entrada.nextFloat();
         
+        //Calculo la nota media
         notaMedia = (pgrmc+lmsgi+bbdd+entdl+ssinf+fol)/6;
+        
+        //Muestro la nota media
         
         System.out.println("Su nota media del curso es de: "+notaMedia);
         
