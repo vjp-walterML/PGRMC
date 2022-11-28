fe6f434512b153ec1e099a545de0ab0b4434b498
vjp-walterML <114687981+vjp-walterML@users.noreply.github.com>
2022-11-28 19:45:40 +0100

    u

--- Tema03/T03Ejercicio15/src/t03ejercicio15/T03Ejercicio15.java
+++ Tema03/T03Ejercicio15/src/t03ejercicio15/T03Ejercicio15.java
@@ -17,14 +17,14 @@ public class T03Ejercicio15 {
      */
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        
+
         //Declaro variables
         int num, resultado;
-        
+
         //Pido el número al usuario para calcular la tabla de multiplicar
         System.out.print("Por favor, introduzca un número para calcular su tabla de multiplicar: ");
         num = entrada.nextInt();
-        
+
         //Utilizo un bucle for para calcular la tabla de multiplicar y mostrarla
         for (int i = 0; i <= 10; i++) {
             resultado = num * i;
