927a5a10d72468a6a7912892d494bcc967e6bf83
vjp-walterML <114687981+vjp-walterML@users.noreply.github.com>
2022-12-14 20:12:53 +0100

    ej

---
+++ Tema04/T04Ejercicio26/src/t04ejercicio26/T04Ejercicio26.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t04ejercicio26;
+
+/**
+ *
+ * @author wmartinl01
+ */
+public class T04Ejercicio26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
