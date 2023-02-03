a9f8fe3c32020c9cfbfd5571842ddbc3d93dff0e
vjp-walterML <114687981+vjp-walterML@users.noreply.github.com>
2023-02-03 16:15:56 +0100

    CASA

--- ENTDL/PruebaComplejidadCiclomatica/src/pruebacomplejidadciclomatica/PruebaComplejidadCiclomatica.java
+++ ENTDL/PruebaComplejidadCiclomatica/src/pruebacomplejidadciclomatica/PruebaComplejidadCiclomatica.java
@@ -14,7 +14,17 @@ public class PruebaComplejidadCiclomatica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        int [] vector = new int [7];
+        int aux;
+        for (int i = 0; i < vector.length-1; i++) {    //NODO1
+            for (int j = 0; j < vector.length-1; j++) {  //NODO2
+                if (vector[j]< vector[j+1]) { //NODO3
+                    aux = vector[j];//NODO4
+                    vector[j] = vector[j+1];//NODO5
+                    vector[j+1] = aux;//NODO6
+                }
+            }//NODO7 FORJ
+        }//NODO8 FORI
+    }//NODO9 MAIN
     
 }
