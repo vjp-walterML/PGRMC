d129730f7a631fbe5f8b6c500cedf843992a7e2a
1DAW - Walter Martin Lopes <wmartinl01@informatica.iesvalledeljerteplasencia.es>
2023-04-12 19:19:07 +0200

    clase

---
+++ Tema12/T12Ejercicio12/src/t12ejercicio12/T12Ejercicio12.java
@@ -0,0 +1,18 @@
+package t12ejercicio12;
+
+import java.io.File;
+
+/**
+ *
+ * @author Walter
+ */
+public class T12Ejercicio12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        File fichero = new File("masnumeros.txt");
+    }
+
+}
