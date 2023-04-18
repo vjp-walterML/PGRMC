9003cd95ce8c7b959173d32e29d9e7537b6e9a74
1DAW - Walter Martin Lopes <wmartinl01@informatica.iesvalledeljerteplasencia.es>
2023-04-18 20:59:54 +0200

    clase

--- Tema12/T12Ejercicio12/src/t12ejercicio12/T12Ejercicio12.java
+++ Tema12/T12Ejercicio12/src/t12ejercicio12/T12Ejercicio12.java
@@ -5,9 +5,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author Walter
